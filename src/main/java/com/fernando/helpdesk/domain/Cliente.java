76594d789122c65b6b19b572369cc88d0420910e
fernando <maruno.string@gmail.com>
2023-09-29 22:23:31 -0300

    anotacoes JPA para classes de dominio

--- src/main/java/com/fernando/helpdesk/domain/Cliente.java
+++ src/main/java/com/fernando/helpdesk/domain/Cliente.java
@@ -1,5 +1,33 @@
 package com.fernando.helpdesk.domain;
 
-public class Cliente {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Cliente extends Pessoa{
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "tecnico")
+	private List<Chamado> chamado = new ArrayList<>();
+
+	public Cliente() {
+		super();
+	}
+
+	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public List<Chamado> getChamado() {
+		return chamado;
+	}
+
+	public void setChamado(List<Chamado> chamado) {
+		this.chamado = chamado;
+	}
+	
+	
 }
